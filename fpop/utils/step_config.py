03861a528417a17785883d53fa52f9058fae30a5
Zezhong Zhang <jack.zezhong.zhang@gmail.com>
2023-10-29 22:19:48 +0100

    enable the dflow debug mode

--- fpop/utils/step_config.py
+++ fpop/utils/step_config.py
@@ -5,7 +5,7 @@ from dflow.plugins.dispatcher import DispatcherExecutor
 def init_executor(
     executor_dict,
 ):
-    if executor_dict is None or config["mode"] == "debug":
+    if executor_dict is None:
         return None
     etype = executor_dict.pop("type")
     if etype == "dispatcher":
